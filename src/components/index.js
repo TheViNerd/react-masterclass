9cf6ad27cfa678e631ba9585d423e53274762ff7
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-10-13 12:46:03 +0530

    Basic Setup

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome to React Masterclass!
+      </div>
+    );
+  }
+}
+
+export default App;
